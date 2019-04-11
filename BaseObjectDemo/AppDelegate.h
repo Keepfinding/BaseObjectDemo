53472147868e362ac42b6c4d38df266874679b10
StephenQin <petee@qq.com>
2019-04-11 13:54:16 +0800

    Initial Commit

---
+++ BaseObjectDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BaseObjectDemo
+//
+//  Created by Stephen Hu on 2019/4/11.
+//  Copyright © 2019 idress. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ BaseObjectDemo/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  BaseObjectDemo
+//
+//  Created by Stephen Hu on 2019/4/11.
+//  Copyright © 2019 idress. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+@end
